6bb70d2af4f64371a462a826b70ef7419a0cac51
mohankumarp <mohan.panigrahi@gmail.com>
2018-11-15 18:09:38 +0530

    added Calc.cs

---
+++ ClassLibrary1/ClassLibrary1/SCICalc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace com.vm
+{
+    public class SCICalc : Calc
+    {
+        public SCICalc()
+        {
+
+        }
+
+        public int SciAdd(int a, int b)
+        {
+            //This is derieved class add
+            return base.Add(a, b);
+
+        }
+    }
+}
