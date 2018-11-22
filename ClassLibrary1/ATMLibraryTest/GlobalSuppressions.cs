6b5c38b95e871ef5a655c97feca5791df695a4cc
mohankumarp <mohan.panigrahi@gmail.com>
2018-11-22 13:30:53 +0530

    added

---
+++ ClassLibrary1/ATMLibraryTest/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1707:Remove the underscores from member name com.vm.test.ATMClassTest.Test_deposit_cash().", Justification = "<Pending>", Scope = "member", Target = "~M:com.vm.test.ATMClassTest.Test_deposit_cash")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1707:Remove the underscores from member name com.vm.test.ATMClassTest.Test_invallid_pin().", Justification = "<Pending>", Scope = "member", Target = "~M:com.vm.test.ATMClassTest.Test_invallid_pin")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1707:Remove the underscores from member name com.vm.test.ATMClassTest.Test_withdraw_cash().", Justification = "<Pending>", Scope = "member", Target = "~M:com.vm.test.ATMClassTest.Test_withdraw_cash")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1707:Remove the underscores from member name com.vm.test.ATMClassTest.Test_withdraw_cash_invalid_denomination().", Justification = "<Pending>", Scope = "member", Target = "~M:com.vm.test.ATMClassTest.Test_withdraw_cash_invalid_denomination")]
---
+++ ClassLibrary1/ClassLibrary1/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1707:In member com.vm.ATMClass.Transaction(int, int, int), remove the underscores from parameter name trans_code.", Justification = "<Pending>", Scope = "member", Target = "~M:com.vm.ATMClass.Transaction(System.Int32,System.Int32,System.Int32)~com.vm.ATMClass.StatusCode")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1822:Member SciAdd does not access instance data and can be marked as static (Shared in VisualBasic)", Justification = "<Pending>", Scope = "member", Target = "~M:com.vm.SCICalc.SciAdd(System.Int32,System.Int32)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1052:Type 'ATMClass' is a static holder type but is neither static nor NotInheritable", Justification = "<Pending>", Scope = "type", Target = "~T:com.vm.ATMClass")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1707:Remove the underscores from member name com.vm.ATMClass.StatusCode.INVALID_DEPOSIT_AMOUNT.", Justification = "<Pending>", Scope = "type", Target = "~T:com.vm.ATMClass.StatusCode")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1052:Type 'Calc' is a static holder type but is neither static nor NotInheritable", Justification = "<Pending>", Scope = "type", Target = "~T:com.vm.Calc")]
